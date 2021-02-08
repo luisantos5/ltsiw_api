faf49e5ccdfa8b0f8d228162fefa0d1e6ec94df0
Luís Santos <luis.costasantos@outlook.com>
2021-02-08 16:06:51 +0000

    BD connection

---
+++ connect.js
@@ -0,0 +1,19 @@
+const mysql = require ('mysql');
+const { SELECT } = require('sequelize/types/lib/query-types');
+module.exports = {
+    con: mysql.createConnection({
+        host: 'cps17.webserver.pt',
+        user: 'grupo1in_user',
+        password: 'Maz3fr2bLsVXSYna',
+        database: 'grupo1in_database'
+    })
+
+    
+
+}
+const connectL = require('./connect');
+connectL.con.query(SELECT * from list_ucs, function (err, rows, fields) {
+    if (!err) {
+        numRows = rows.lenght;
+    }
+})
