05b6f716e619b947ada53074891109ffb14f9d3c
Luís Santos <luis.costasantos@outlook.com>
2021-02-08 15:37:36 +0000

    BD connected

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.listen(port, () => {
     console.log("Servidor a correr na porta " + port)
 })
 
-// tenho de ligar a base de dados ao site
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('grupo1in_database', 'grupo1in_user', 'Maz3fr2bLsVXSYna', {
+    host: 'cps17.webserver.pt',
+    dialect: 'mysql', 
+});
+
+
